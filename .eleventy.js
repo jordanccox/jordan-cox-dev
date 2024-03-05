8e41971e6c9b2bf64bcecebc6138bff52a36b556
Jordan Cox <jordancox747@outlook.com>
2024-03-05 14:59:12 -0700

    install lightbox plugin

--- .eleventy.js
+++ .eleventy.js
@@ -24,6 +24,7 @@ module.exports = function (config) {
   config.addNunjucksAsyncFilter("postcss", postcssFilter);
   config.addPassthroughCopy("./media");
   config.addPassthroughCopy("./src/bundle.js");
+  config.addPassthroughCopy("./src/fslightbox.js");
   config.addPlugin(eleventyNavigationPlugin);
 
   return {
